41ab46a9bc0730913b2bc8eeccf540f12446e332
Victor Leite <victor_leite@outlook.com.br>
2020-01-13 21:33:44 -0300

    Interceptando os erros da API

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -21,7 +21,7 @@ export class NavComponent implements OnInit {
       this.model.password = null;
       console.log('Logged in successfully');
     }, error => {
-      console.log('Failed to Login');
+      console.log(error);
     });
   }
 
