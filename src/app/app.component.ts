abfc96c9075d8752267bc395476f0bfd97a251c5
Victor Leite <victor_leite@outlook.com.br>
2020-01-14 20:22:33 -0300

    Resolvendo token e colocando nome de usuario na barra superior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './_services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'DatingApp-SPA';
+  
+  /**
+   *
+   */
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.authService.loadDecodedToken();
+  }
 }
